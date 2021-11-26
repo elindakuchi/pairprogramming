8151c44c6adebfbd770e6a0bd559ddcd2dfb4133
Kuci <elinda.kuci@sap.com>
2021-11-26 13:40:37 +0100

    Second Requirement done

--- greet.test.js
+++ greet.test.js
@@ -1,9 +1,17 @@
-function greet(name){
-    return `Hello, ${name}`
+
+function greet(name =null){
+    if( name !==null) return `Hello, ${name}`
+
+    return "Hello, my friend"
+    
 }
 
 describe("greet(): ",()=>{
     test("should display Hello, Bob when it is called with argument Bob",() => {
         expect(greet('Bob')).toStrictEqual('Hello, Bob');
+    } )
+
+    test("should display Hello, my friend when it null",() => {
+        expect(greet()).toEqual('Hello, my friend');
     } )
 })
